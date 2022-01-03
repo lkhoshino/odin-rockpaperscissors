03ef511860366c088a81847136e6f1cbf790a9e2
lkhoshino <lucaskenzoh@gmail.com>
2022-01-03 15:25:27 -0300

    functional game round

--- scripts.js
+++ scripts.js
@@ -1,20 +1,68 @@
+let computerChoice;
+let playerChoice;
+
 function computerPlay() {
     let x;
     x = Math.floor(Math.random() * 3); 
     if(x==0){
-        console.log("Rock");
+        return computerChoice = 0
     }
     if(x==1){
-        console.log("Paper");
+        return computerChoice = 1
     }
     if(x==2){
-        console.log("Scissors");
+         return computerChoice = 2
     }
 }
 
 function playerSelection() {
-    let choice = prompt().toLowerCase;
+    let choice = prompt("Rock, paper or scissors?").toLowerCase();
+    if(choice =="rock"){
+        return playerChoice = 0 
+    }
+    if(choice =="paper"){
+        return playerChoice = 1
+    }
+    if(choice =="scissors"){
+        return playerChoice = 2
+    }
+}
+
+function playRound(){
+    if(computerChoice == playerChoice){
+        console.log("Draw!");
+    }
+    if(computerChoice == 0 && playerChoice == 1){
+        console.log("You win! Paper beats rock");
+    }
+    if(computerChoice == 0 && playerChoice == 2){
+        console.log("You lose! Rock beats scissors");
+    }
+    if(computerChoice == 1 && playerChoice == 0){
+        console.log("You lose! Paper beats rock")
+    }
+    if(computerChoice == 1 && playerChoice == 2){
+        console.log("You win! Scissors beats paper")
+    }
+    if(computerChoice == 2 && playerChoice == 0){
+        console.log("You win! Rock beats scissors")
+    }
+    if(computerChoice == 2 && playerChoice == 1){
+        console.log("You lose! Scissors beats paper")
+    }
+}
+function game(){
+    let computerScore = 0;
+    let playerScore = 0;
+
+    while(computerScore <5 || playerScore<5){
+        playRound();
+    }
+
 }
 
 computerPlay();
 playerSelection();
+playRound();
+console.log(computerChoice);
+console.log(playerChoice);
